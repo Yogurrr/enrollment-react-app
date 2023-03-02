a8ffa3747e092e3a77e8798df9e15f1e8d68a16e
LeeSeungYeon <masakisqq@naver.com>
2023-03-02 17:10:37 +0900

    state를 이용한 환영 메시지 출력

--- src/components/EnrollmentForm.js
+++ src/components/EnrollmentForm.js
@@ -1,16 +1,35 @@
 import React from "react";
+import { useState } from "react";
 import '../App.css';
 
 const EnrollmentForm = () => {
+    const [firstName, setFirstName] = useState("");   // 인풋 태그에 작성했던 내용을 폼에 저장하기 위해
+    const [lastName, setLastName] = useState("");
+    const [welcomeMessage, setWelcomeMessage] = useState("");
+
+    const handleSubmit = (e) => {
+        setWelcomeMessage(`환영합니다 ${firstName} ${lastName}님!!`);
+        e.preventDefault();   // submit 기능 중지
+    };
     return (
         <div>
-            <form className="enrolForm"></form>
-            <h1>대학생 등록 양식</h1>
-            <div><label>First Name</label>
-                <input type="text" name="fname" /></div>
-            <div><label>Last Name</label>
-                <input type="text" name="lname" /></div>
-            <div><button type="submit">등록하기</button></div>
+            <form className="enrolForm" onSubmit={handleSubmit}>
+                <h1>대학생 등록 양식</h1>
+                <div><label>First Name</label>
+                    <input type="text" name="fname"
+                           onBlur={ (e) => setFirstName(e.target.value) } /></div>
+
+                <div><label>Last Name</label>
+                    <input type="text" name="lname"
+                           onBlur={ (e) => setLastName(e.target.value) } /></div>
+
+                <div><button type="submit"
+                             onBlur={ (e) => setWelcomeMessage(e.target.value) }>등록하기</button></div>
+            </form>
+
+            <label id="studentMsg" className="message">
+                {welcomeMessage}
+            </label>
         </div>
     );
 };
